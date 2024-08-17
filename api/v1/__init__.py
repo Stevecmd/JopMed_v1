0c855290a6762e77b434c13d99a15d564d2d2754
SteveCmd <murimi101@gmail.com>
2024-08-17 18:06:46 +0300

    chore: Add initial API setup and blueprints

---
+++ api/v1/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask  
+from flask_cors import CORS  
+from flasgger import Swagger  
+  
+app = Flask(__name__)  
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True  
+cors = CORS(app, resources={r"/*": {"origins": "*"}})  
+app.config['SWAGGER'] = {  
+   'title': 'JOPMED Restful API',  
+   'uiversion': 3  
+}  
+Swagger(app)
