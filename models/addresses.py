93719e4567a393c5a53c2f28d1f1b04d7ff03f53
SteveCmd <murimi101@gmail.com>
2024-08-19 17:24:25 +0300

    chore: Update Addresses model with database columns
    The code changes include updating the Addresses model in the addresses.py file. The model now includes database columns for user_id, city, country, zip_code, street_address, phone_number, created_at, and updated_at. This allows for storing and retrieving address information from the database.
    
    Based on the recent user commits and repository commits, the suggested commit message follows the convention of starting with a type (chore for maintenance) followed by a concise description of the changes made.

--- models/addresses.py
+++ models/addresses.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python3
 """ holds class Addresses"""
+import datetime
 import models
 from models.base_model import BaseModel, Base
-# from models.city import City
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 
 
 class Addresses(BaseModel, Base):
     """Representation of Addresses """
-    # if models.storage_t == "db":
-    #     __tablename__ = 'states'
-    #     id = Column(String(60), primary_key=True, nullable=False)
-    #     name = Column(String(128), nullable=False)
-    #     cities = relationship("City",
-    #                           backref="state",
-    #                           cascade="all, delete, delete-orphan")
-    # else:
-    #     name = ""
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        id = Column(String(60), primary_key=True, nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        city = Column(String(255), nullable=False)
+        country = Column(String(255), nullable=False)
+        zip_code = Column(String(255), nullable=False)
+        street_address = Column(String(255), nullable=False)
+        phone_number = Column(String(255))
+        created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+        updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    else:
+        name = ""
 
-    # def __init__(self, *args, **kwargs):
-    #     """initializes state"""
-    #     super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
 
     # if models.storage_t != "db":
-    #     @property
-    #     def cities(self):
-    #         """getter for list of city instances related to the state"""
-    #         city_list = []
-    #         all_cities = models.storage.all(City)
-    #         for city in all_cities.values():
-    #             if city.state_id == self.id:
-    #                 city_list.append(city)
-    #         return city_list
+        # @property
+        # def cities(self):
+        #     """getter for list of city instances related to the state"""
+        #     city_list = []
+        #     all_cities = models.storage.all(City)
+        #     for city in all_cities.values():
+        #         if city.state_id == self.id:
+        #             city_list.append(city)
+        #     return city_list
