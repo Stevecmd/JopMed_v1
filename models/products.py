5be15373d7e6e01c9e40ffab67c27f3c6fa02f3a
SteveCmd <murimi101@gmail.com>
2024-08-20 10:19:04 +0300

    chore: Update Products model with additional columns
    The code changes include updating the Products model in the products.py file.
    
    - Added columns for name, description, price, stock, category, slug, created_at, and updated_at
    - Updated the length of name and category columns
    - Added a relationship with the Comments model
    
    These changes allow for storing and retrieving product information from the database.

--- models/products.py
+++ models/products.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python
-""" holds class Place"""
+""" holds class Products"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table, Text, DateTime, DECIMAL
 from sqlalchemy.orm import relationship
-
-if models.storage_t == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
-                                 primary_key=True),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
-                                 primary_key=True))
+from datetime import datetime
 
 
 class Products(BaseModel, Base):
     """Representation of Products """
-comments = relationship("Comments", back_populates="product")
+    if models.storage_t == 'db':
+        __tablename__ = 'products'
+        name = Column(String(255), nullable=False)
+        description = Column(Text)
+        price = Column(DECIMAL(10, 2), nullable=False)
+        stock = Column(Integer, nullable=False)
+        category = Column(String(255))
+        slug = Column(String(255))
+        created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+        updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+        comments = relationship("Comments", back_populates="product")
